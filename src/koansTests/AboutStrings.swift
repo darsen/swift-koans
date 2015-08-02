183e0e5301bc86ec2f07f6c93d72539e2410c4be
Danijel Arsenovski <darsenovski@darsenovski-mp1.local>
2015-08-01 20:20:56 -0700

    More string koans

--- src/koansTests/AboutStrings.swift
+++ src/koansTests/AboutStrings.swift
@@ -4,10 +4,37 @@ import XCTest
 
 class AboutStrings: KoanTest {
     
+    func test_EmptyString(){
+        let emptyFromInitializer = String()
+        let notEmpty = "Not empty"
+        XCTAssertEqual( notEmpty, emptyFromInitializer, "replace 'Not empty' to make it work")
+    }
+    
+    func test_IsEmptyString(){
+        let str = "Not empty"
+        let isEmpty = str.isEmpty
+        XCTAssertEqual( ____, isEmpty, "replace ____ to make it work")
+    }
+    
+    func test_StringConcatenation(){
+        var money = "$"
+        money += money
+        let world = money + "\(money) makes the world go around"
+        XCTAssertEqual(__ , world, "replace __ to make it work")
+    }
+    
     func test_StringInterpolation(){
         let money = "$"
         let world = "\(money)\(money)\(money) makes the world go around"
         XCTAssertEqual(__ , world, "replace __ to make it work")
     }
+    
+    func test_StringInterpolationWithExpression(){
+        let money = "$"
+        let world = "\(money + money + money) makes the world"
+        XCTAssertEqual(__ , world, "replace __ to make it work")
+    }
+    
+    
 
 }
