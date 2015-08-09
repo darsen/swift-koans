e078eeee52e72949fb0256178ff037b4d35ff635
Danijel Arsenovski <darsenovski@darsenovski-mp1.local>
2015-08-08 21:27:16 -0700

    more array koans

--- src/koansTests/AboutArrays.swift
+++ src/koansTests/AboutArrays.swift
@@ -13,10 +13,44 @@ class AboutArrays: KoanTest{
         XCTAssertEqual(______ , numbers[2], "replace _____ to make it work")
     }
     
+    func test_ArrayInitializerWithRepeatedValue(){
+        let numbers = [Int](count: 77, repeatedValue: 7)
+        XCTAssertEqual(______ , numbers[76], "replace _____ to make it work")
+    }
+    
+    func test_IsEmpty(){
+        var numbers = [3,4,5]
+        XCTAssertEqual(____ , numbers.isEmpty, "replace __ to make it work")
+    }
+    
     func test_AppendAndLast(){
         var numbers = [3,4,5]
         numbers.append(6)
         XCTAssertEqual(______ , numbers.last!, "replace __ to make it work")
+    }
+    
+    func test_Insert(){
+        var numbers = [3,4,5]
+        numbers.insert(6, atIndex: 2)
+        XCTAssertEqual(______ , numbers[2], "replace __ to make it work")
+    }
+    
+    func test_Remove(){
+        var numbers = [3,4,5]
+        numbers.removeAtIndex(1)
+        XCTAssertEqual(______ , numbers[1], "replace __ to make it work")
+    }
+    
+    func test_RemoveReducesItemCount(){
+        var numbers = [3,4,5]
+        numbers.removeAtIndex(1)
+        XCTAssertEqual(______ , numbers.count, "replace __ to make it work")
+    }
+    
+    func test_RemoveReturnsItem(){
+        var numbers = [3,4,5]
+        let removed = numbers.removeAtIndex(1)
+        XCTAssertEqual(______ , removed, "replace __ to make it work")
     }
     
     func test_Filter(){
@@ -31,5 +65,17 @@ class AboutArrays: KoanTest{
         XCTAssertEqual(______ , numbers[0], "replace _____ to make it work")
     }
     
+    func test_AddArrays(){
+        let even = [2,4,6]
+        let odd = [3,5,7]
+        let evenAndOdd = even + odd
+        XCTAssertEqual(______ , evenAndOdd[1], "replace _____ to make it work")
+    }
+    
+    func test_ReferenceRangeOfValues(){
+        var even = [2,4,6]
+        even[0...2] = [3,5]
+        XCTAssertEqual([______, ______] , even, "replace ______ to make it work")
+    }
     
 }
