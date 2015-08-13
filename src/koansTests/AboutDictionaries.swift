19d11b441ea059ad3f7778668eb9610ceb08ca17
Danijel Arsenovski <darsenovski@darsenovski-mp1.local>
2015-08-13 13:00:57 -0700

    more dictionary koans

--- src/koansTests/AboutDictionaries.swift
+++ src/koansTests/AboutDictionaries.swift
@@ -3,10 +3,22 @@ import XCTest
 
 class AboutDictionaries: KoanTest {
 
-    func test_InitializeDictionaryFromLiteral(){
-        
+    func test_InitializeDictionaryFromLiteral(){        
         let airportCodes = [__:"SCL", "Seattle": __]
         XCTAssertEqual( airportCodes, ["Santiago de Chile":"SCL", "Seattle": "SEA"], "replace __ to make it work")
     }
+    
+    func test_IsEmpty(){
+        var airportCodes :[String: String] = [:]
+        XCTAssertEqual(___, airportCodes.isEmpty, "replace ___ to make it work")
+    }
 
+    
+    func test_DeclareAndAddDataToDictionary(){
+        var airportCodes :[String: String] = [:]
+        airportCodes["Santiago de Chile"] = "SCL"
+        airportCodes["Seattle"] = "SEA"
+        XCTAssertEqual( [__:"SCL", "Seattle": __], airportCodes, "replace __ to make it work")
+    }
+    
 }
