045d8d14a11ac87430fa2496dd7a49841ee839bc
Danijel Arsenovski <darsenovski@darsenovski-mp1.local>
2015-08-17 15:43:54 -0700

    switching to xcode 7

--- src/koansTests/AboutDictionaries.swift
+++ src/koansTests/AboutDictionaries.swift
@@ -27,4 +27,6 @@ class AboutDictionaries: KoanTest {
     }
     
     
+    
+    
 }
--- src/koansTests/AboutErrorHandling.swift
+++ src/koansTests/AboutErrorHandling.swift
@@ -1,9 +1,23 @@
-//
-//  AboutErrorHandling.swift
-//  koans
-//
-//  Created by Danijel Arsenovski on 8/17/15.
-//  Copyright (c) 2015 Danijel Arsenovski. All rights reserved.
-//
-
 import Foundation
+import XCTest
+
+class AboutErrorHandling: KoanTest{
+    
+    func test_CatchingError() {
+        var filePath = NSBundle.mainBundle().pathForResource("Do not exist", ofType:"txt")
+        do{
+            var data = try NSData(contentsOfFile:filePath!)
+        } catch {
+            println("hi")
+        }
+    }
+    
+    func test_CatchingError2() {
+        var filePath = NSBundle.mainBundle().pathForResource("Do not exist", ofType:"txt")
+        
+        var data = NSData(contentsOfFile:filePath!)
+        println("")
+    
+    }
+
+}
