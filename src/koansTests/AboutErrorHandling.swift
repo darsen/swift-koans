c204917781daed21a7351315d2ef3deccb73266e
Danijel Arsenovski <darsenovski@darsenovski-mp1.local>
2015-08-17 15:42:49 -0700

    switching to xcode 7

---
+++ src/koansTests/AboutErrorHandling.swift
@@ -0,0 +1,9 @@
+//
+//  AboutErrorHandling.swift
+//  koans
+//
+//  Created by Danijel Arsenovski on 8/17/15.
+//  Copyright (c) 2015 Danijel Arsenovski. All rights reserved.
+//
+
+import Foundation
