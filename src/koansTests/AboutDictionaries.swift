c36576a8c356a2b96453e0b1bceb2de0088b5d71
Danijel Arsenovski <darsenovski@darsenovski-mp1.local>
2015-08-17 13:06:51 -0700

    more dictionary koans

--- src/koansTests/AboutDictionaries.swift
+++ src/koansTests/AboutDictionaries.swift
@@ -8,17 +8,23 @@ class AboutDictionaries: KoanTest {
         XCTAssertEqual( airportCodes, ["Santiago de Chile":"SCL", "Seattle": "SEA"], "replace __ to make it work")
     }
     
+    func test_DeclareDictionaryTypesExplicitly(){
+        let airportCodes:[String: String] = [__:"SCL", "Seattle": __]
+        XCTAssertEqual( airportCodes, ["Santiago de Chile":"SCL", "Seattle": "SEA"], "replace __ to make it work")
+    }
+    
     func test_IsEmpty(){
-        var airportCodes :[String: String] = [:]
+        var airportCodes:[String: String] = [:]
         XCTAssertEqual(___, airportCodes.isEmpty, "replace ___ to make it work")
     }
 
     
-    func test_DeclareAndAddDataToDictionary(){
-        var airportCodes :[String: String] = [:]
+    func test_AddDataToDictionary(){
+        var airportCodes:[String: String] = [:]
         airportCodes["Santiago de Chile"] = "SCL"
         airportCodes["Seattle"] = "SEA"
         XCTAssertEqual( [__:"SCL", "Seattle": __], airportCodes, "replace __ to make it work")
     }
+    
     
 }
