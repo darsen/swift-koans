da3200a809e63db229d441e6a76a2c86fed5bb74
Danijel Arsenovski <darsenovski@darsenovski-mp1.local>
2015-08-18 09:53:29 -0700

    more error handling koans

--- src/koansTests/AboutErrorHandling.swift
+++ src/koansTests/AboutErrorHandling.swift
@@ -3,10 +3,30 @@ import XCTest
 
 class AboutErrorHandling: KoanTest{
     
+    
     func test_CatchingError() {
+        var errorMessage = ""
+        let data = "&^%".dataUsingEncoding(NSUTF8StringEncoding)
+        do {
+            let object : AnyObject = try NSJSONSerialization.JSONObjectWithData(data!, options: [])
+            print(object)
+        } catch let error {
+            if   error._domain == NSCocoaErrorDomain
+                && error._code   == 3840 {
+                    errorMessage = "Invalid format"
+            } else {
+                errorMessage = "surprise!"
+            }
+        }
+        XCTAssertEqual(__, errorMessage, "replace __ to make it work")
+    }
+    
+    func test_CatchingCustomError() {
+        
         enum MyErrorEnum : ErrorType {
           case Example
         }
+        
         var errorMessage = ""
         do{
             throw MyErrorEnum.Example
