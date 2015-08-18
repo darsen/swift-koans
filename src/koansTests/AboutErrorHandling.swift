6cdd2d69ec824e03001eaea43b675fb7c77707b0
Danijel Arsenovski <darsenovski@darsenovski-mp1.local>
2015-08-18 09:38:56 -0700

    error handling koan

--- src/koansTests/AboutErrorHandling.swift
+++ src/koansTests/AboutErrorHandling.swift
@@ -4,7 +4,17 @@ import XCTest
 class AboutErrorHandling: KoanTest{
     
     func test_CatchingError() {
-
+        enum MyErrorEnum : ErrorType {
+          case Example
+        }
+        var errorMessage = ""
+        do{
+            throw MyErrorEnum.Example
+        }
+        catch{
+            errorMessage = "caught"
+        }
+        XCTAssertEqual(__, errorMessage, "replace __ to make it work")
     }
 
 }
