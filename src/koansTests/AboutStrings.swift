e39907c404bc58f395069b405d1661ef29e1b96a
Megan McPherson <meganm@MSEAVXPBOUDREAU.local>
2016-07-05 15:11:30 -0700

    added four koans

--- src/koansTests/AboutStrings.swift
+++ src/koansTests/AboutStrings.swift
@@ -16,11 +16,15 @@ class AboutStrings: KoanTest {
         XCTAssertEqual( ____, isEmpty, "replace ____ to make it work")
     }
     
-    func test_StringConcatenation(){
-        var money = "$"
-        money += money
-        let world = money + "\(money) makes the world go around"
-        XCTAssertEqual(__ , world, "replace __ to make it work")
+    func test_StringConstructionUsingCharacterArray(){
+        let petCharacters: [Character] = ["🐱", "s", " ", "a", "n", "d", " ", "🐶", "s"]
+        let petString = String(petCharacters)
+        XCTAssertEqual(__, petString, "replace __ to make it work")
+    }
+    
+    func test_StringCountingCharacters(){
+        let pets = "🐱s and 🐶s"
+        XCTAssertEqual(______, pets.characters.count, "replace ______ to make it work")
     }
     
     func test_StringCharacterConcatenation(){
@@ -35,6 +39,22 @@ class AboutStrings: KoanTest {
         XCTAssertEqual(__ , gettingAlong, "replace __ to make it work")
     }
     
+    func test_StringCharacterConcatenationWithForInLoop(){
+        let pets = "🐱s and 🐶s"
+        var weather = "raining like "
+        for character in pets.characters{
+            weather.append(character)
+        }
+        XCTAssertEqual(__ , weather, "replace __ to make it work")
+    }
+    
+    func test_StringConcatenation(){
+        var money = "$"
+        money += money
+        let world = money + "\(money) makes the world go around"
+        XCTAssertEqual(__ , world, "replace __ to make it work")
+    }
+    
     func test_StringInterpolation(){
         let money = "$"
         let world = "\(money)\(money)\(money) makes the world go around"
@@ -43,10 +63,14 @@ class AboutStrings: KoanTest {
     
     func test_StringInterpolationWithExpression(){
         let money = "$"
-        let world = "\(money + money + money) makes the world"
-        XCTAssertEqual(__ , world, "replace __ to make it work")
+        let world = "\(money + money + money) makes the world go around"
+        XCTAssertEqual(__, world, "replace __ to make it work")
     }
     
+    func test_StringAccessUsingIndices(){
+        let world = "$$$ makes the world go around"
+        XCTAssertEqual(__, String(world[world.startIndex]), "replace __ to make it work")
+        XCTAssertEqual(__, String(world[world.endIndex.predecessor()]), "replace __ to make it work")
+    }
     
-
 }
